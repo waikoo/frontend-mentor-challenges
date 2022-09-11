af49682877cd36847c083e8cdeb83e199137b37a
wexxer <96206008+wexxer@users.noreply.github.com>
2022-09-11 23:39:11 +0300

    Add preliminary JS validation

--- app.js
+++ app.js
@@ -7,15 +7,23 @@ const yearEl = document.querySelector('#expiry_year');
 const cvcEl = document.querySelector('#cvc');
 const submitBtnEl = document.querySelector('.card__form--btn');
 
-// cardNumberEl.value
+const errName = document.querySelector('.error-name');
+
+const cardName = document.querySelector('.card-name');
+const cardNumber = document.querySelector('.card-nr');
+const cardMonth = document.querySelector('.card-month');
+const cardYear = document.querySelector('.card-year');
+const cardCvc = document.querySelector('.cvc');
 
 formEl.addEventListener('submit', e => {
 	e.preventDefault();
 
-	// check for name not being empty on submit
+	// if name field is empty, make error message visible
 	const isNameCompleted = () => {
 		if (!nameEl.value) {
-			document.querySelector('.error-name').classList.remove('invisible');
+			errName.classList.remove('invisible');
+			nameEl.classList.add('focus-invalid');
+			nameEl.focus();
 		} else {
 			return true;
 		}
@@ -24,10 +32,12 @@ formEl.addEventListener('submit', e => {
 	const isCardNumberValid = () => {
 		// if there is an input value, check it with regex:
 		if (cardNumberEl.value) {
-			return /^[0-9]{16}$/.test(cardNumberEl.value);
+			return /^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$/.test(cardNumberEl.value);
 		} else {
 			//if input field is empty:
 			document.querySelector('.error-number').classList.remove('invisible');
+			cardNumberEl.classList.add('focus-invalid');
+			cardNumberEl.focus();
 		}
 	};
 
@@ -37,6 +47,8 @@ formEl.addEventListener('submit', e => {
 			return monthRegex.test(monthEl.value);
 		} else {
 			document.querySelector('.error-expdate').classList.remove('invisible');
+			monthEl.classList.add('focus-invalid');
+			monthEl.focus();
 		}
 	};
 
@@ -44,22 +56,23 @@ formEl.addEventListener('submit', e => {
 		if (yearEl.value) {
 			return /^\d{2}$/.test(yearEl.value);
 		} else {
-			document.querySelector('.error-cvc').classList.remove('invisible');
+			document.querySelector('.error-expdate').classList.remove('invisible');
+			yearEl.classList.add('focus-invalid');
+			yearEl.focus();
 		}
 	};
 
 	const isCvcValid = () => {
 		if (cvcEl.value) {
 			return /^\d{3}$/.test(cvcEl.value);
+		} else {
+			document.querySelector('.error-cvc').classList.remove('invisible');
+			cvcEl.classList.add('focus-invalid');
+			cvcEl.focus();
 		}
 	};
 
 	//final evaluation on submit
-	const nameValidated = isNameCompleted();
-	const cardNumberValidated = isCardNumberValid();
-	const expMonthValidated = isExpMonthValid();
-	const expYearValidated = isExpYearValid();
-	const cvcValidated = isCvcValid();
 
 	console.log('name: ' + isNameCompleted());
 	console.log('card: ' + isCardNumberValid());
@@ -68,18 +81,73 @@ formEl.addEventListener('submit', e => {
 	console.log('cvc: ' + isCvcValid());
 
 	if (
-		nameValidated &&
-		cardNumberValidated &&
-		expMonthValidated &&
-		expYearValidated &&
-		cvcValidated
+		isNameCompleted() &&
+		isCardNumberValid() &&
+		isExpMonthValid() &&
+		isExpYearValid() &&
+		isCvcValid()
 	) {
 		formEl.submit();
 	}
 });
 
 nameEl.addEventListener('keyup', () => {
+	cardName.textContent = nameEl.value;
 	if (nameEl.value) {
 		document.querySelector('.error-name').classList.add('invisible');
+		nameEl.classList.remove('focus-invalid');
+	}
+});
+
+cardNumberEl.addEventListener('keyup', () => {
+	cardNumber.textContent = cardNumberEl.value;
+	const cardNumberValueValid = /^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$/.test(
+		cardNumberEl.value,
+	);
+	if (cardNumberValueValid) {
+		document.querySelector('.error-number').classList.add('invisible');
+	}
+	if (!cardNumberValueValid) {
+		document.querySelector('.error-number').classList.remove('invisible');
+		cardNumberEl.classList.remove('focus-invalid');
+	}
+});
+
+monthEl.addEventListener('keyup', () => {
+	cardMonth.textContent = monthEl.value;
+	const monthValueValid = /01|02|03|04|05|06|07|08|09|10|11|12/.test(
+		monthEl.value,
+	);
+	if (monthValueValid) {
+		document.querySelector('.error-expdate').classList.add('invisible');
+	}
+	if (!monthValueValid) {
+		document.querySelector('.error-expdate').classList.remove('invisible');
+		monthEl.classList.remove('focus-invalid');
+	}
+});
+
+yearEl.addEventListener('keyup', () => {
+	cardYear.textContent = yearEl.value;
+	const yearValueValid = /^\d{2}$/.test(yearEl.value);
+	if (yearValueValid && yearEl.value.length > 1) {
+		document.querySelector('.error-expdate').classList.add('invisible');
+	}
+	if (!yearValueValid) {
+		document.querySelector('.error-expdate').classList.remove('invisible');
+		yearEl.classList.remove('focus-invalid');
+	}
+});
+
+cvcEl.addEventListener('keyup', () => {
+	cardCvc.textContent = cvcEl.value;
+	console.log(cvcEl.length);
+	const cvcValueValid = /^\d{3}$/.test(cvcEl.value);
+	if (cvcValueValid) {
+		document.querySelector('.error-cvc').classList.add('invisible');
+	}
+	if (!cvcValueValid) {
+		document.querySelector('.error-cvc').classList.remove('invisible');
+		cvcEl.classList.remove('focus-invalid');
 	}
 });
