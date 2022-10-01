49a74d0670d2ba65fa51811e3c091cd8a8f08ac8
waikoo <b4rn1k4@yahoo.com>
2022-10-01 18:30:57 +0300

    Refactor JS

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-function $(selector) {
-	return document.querySelector(selector);
-}
-function $$(classSelector) {
-	return document.body.getElementsByClassName(classSelector);
-}
+import { $, $$ } from './js/util.js';
+
 function errorMessage(command) {
 	switch (command) {
 		case 'show':
@@ -21,99 +17,88 @@ function errorMessage(command) {
 			break;
 	}
 }
-function fixTipHandler(button, bill) {
-	if (bill.isCustomTip) {
-		bill.isCustomTip = false;
-		bill.isFixTip = true;
-	} else {
-		for (let fixTip of $$('fixedTip')) {
-			if (button === fixTip) {
-				button.classList.add('selected');
-				const percentage = parseInt(button.textContent);
-				return (bill.total * percentage) / 100;
-			}
-		}
-	}
+function fixTipHandler(bill) {
+	return (bill.total * bill.tipPercentage) / 100;
 }
 function getTotalPerPerson(bill, tip, people) {
-	return (bill + tip) / people;
+	const result = (bill + tip) / people;
+	if (!Number.isFinite(result)) return 0;
+	return result;
 }
 function getTipPerPerson(calculatedTip, nrOfPeople) {
-	return calculatedTip / nrOfPeople;
+	const result = calculatedTip / nrOfPeople;
+	if (!Number.isFinite(result)) return 0;
+	return result;
 }
-function displayResult(obj) {
-	console.log(obj);
-	obj.result = {};
-
-	obj.result.withTip = getTotalPerPerson(
-		obj.total,
-		obj.tipValue,
-		obj.nrOfPeople
-	);
-
-	obj.result.tipOnly = getTipPerPerson(obj.tipValue, obj.nrOfPeople);
-
-	console.log(obj.result.tipOnly);
-	console.log(obj.result.withTip);
-	$('.tip-only').textContent = `$${obj.result.tipOnly.toFixed(2)}`;
-	$('.with-tip').textContent = `$${obj.result.withTip.toFixed(2)}`;
-}
-
-window.addEventListener('DOMContentLoaded', function () {
-	const bill = {
-		isCustomTip: false,
-		isFixTip: false
+function displayResult(bill) {
+	bill.tipValue = fixTipHandler(bill);
+	const result = {
+		withTip: getTotalPerPerson(
+			bill.total,
+			bill.tipValue,
+			bill.nrOfPeople
+		).toFixed(2),
+		tipOnly: getTipPerPerson(bill.tipValue, bill.nrOfPeople).toFixed(2)
 	};
 
-	$('#bill').addEventListener('change', e => {
-		let registerString = '';
-		registerString += e.currentTarget.value;
-		bill.valueOnInput = Number(registerString);
-	});
+	$('.tip-only').textContent = `$${result.tipOnly}`;
+	$('.with-tip').textContent = `$${result.withTip}`;
+}
 
-	$('#bill').addEventListener('focusout', () => {
-		bill.valueOnFocusOut = Number($('#bill').value);
-		$('#bill').value && $('#bill').classList.add('validated');
-		if (bill.valueOnInput === bill.valueOnFocusOut) {
-			bill.total = bill.valueOnInput;
-			delete bill.valueOnInput;
-			delete bill.valueOnFocusOut;
-		}
-	});
+const billEl = document.querySelector('#bill');
+const customTipEl = document.querySelector('.customTip');
 
-	$('.customTip').addEventListener('focusout', () => {
-		$('.customTip').value && $('.customTip').classList.add('validated');
-	});
+const bill = {
+	total: null,
+	tipValue: null,
+	tipPercentage: null,
+	nrOfPeople: null
+};
 
-	for (let tip of $$('fixedTip')) {
-		tip.addEventListener('click', function () {
-			bill.isFixTip = true;
-			bill.tipValue = fixTipHandler(this, bill);
-		});
+customTipEl.addEventListener('focusout', () => {
+	if ($('#people').value.length < 1) {
+		errorMessage('show');
 	}
+});
 
-	$('.customTip').addEventListener('change', e => {
-		if ($('.customTip').value) {
-			bill.isCustomTip = true;
-			bill.isFixTip = false;
-			let customNum = Number(e.currentTarget.value);
-			bill.tipValue = (bill.total * customNum) / 100;
+for (let tipEl of $$('fixedTip')) {
+	tipEl.addEventListener('click', function (e) {
+		const tipElement = e.target;
+
+		if (tipElement === tipEl) {
+			tipEl.classList.add('selected');
+		} else {
+			tipEl.classList.remove('selected');
 		}
-	});
-
-	$('.customTip').addEventListener('focusout', () => {
-		if ($('#people').value.length < 1) {
-			errorMessage('show');
-		}
-	});
-
-	$('#people').addEventListener('input', function (e) {
-		$('#people').value === 0
-			? errorMessage('show')
-			: errorMessage('disappear');
-
-		bill.nrOfPeople = parseInt(e.data);
-
+		bill.tipPercentage = Number(tipElement.dataset.percent);
 		displayResult(bill);
 	});
+}
+
+billEl.addEventListener('input', e => {
+	const registerString = e.currentTarget.value;
+	bill.total = Number(registerString);
+
+	displayResult(bill);
+});
+
+customTipEl.addEventListener('input', e => {
+	if (!customTipEl.value) return;
+	let customNum = Number(e.currentTarget.value);
+	bill.tipPercentage = customNum;
+
+	displayResult(bill);
+});
+
+customTipEl.addEventListener('focusout', () => {
+	if ($('#people').value.length < 1) {
+		errorMessage('show');
+	}
+});
+
+$('#people').addEventListener('input', function (e) {
+	$('#people').value === 0 ? errorMessage('show') : errorMessage('disappear');
+
+	bill.nrOfPeople = parseInt(e.data);
+	displayResult(bill);
 });
---
+++ js/util.js
@@ -0,0 +1,6 @@
+export function $(selector) {
+	return document.querySelector(selector);
+}
+export function $$(classSelector) {
+	return document.getElementsByClassName(classSelector);
+}
