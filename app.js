c3bf2c254a9cfbd082e7707f6d7cd13cd0d7ff77
waikoo <b4rn1k4@yahoo.com>
2022-11-14 00:27:16 +0200

    Add new todo

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const body = document.body;
 const darkTheme = window.matchMedia('(prefers-color-scheme: dark)');
 let currentTheme = null;
 
+const tabs = document.querySelectorAll('.tabs');
+const form = document.querySelector('form');
+const todoText = document.querySelector('input');
+const todosContainer = document.querySelector('.todos-con');
+
 function $(selector) {
 	return document.querySelector(selector);
 }
@@ -13,13 +18,11 @@ function setPreferredColorScheme() {
 	darkTheme.matches ? changeThemeTo('dark') : changeThemeTo('light');
 }
 
-setPreferredColorScheme();
-
 function handleThemeToggler() {
 	currentTheme === 'light' ? changeThemeTo('dark') : changeThemeTo('light');
 }
 
-function changeBgOfElementTo(element, color) {
+function setBgOfElementTo(element, color) {
 	// light theme
 	let localColor;
 	if (color === 'alabaster') localColor = 'hsl(0, 0%, 98%)';
@@ -39,7 +42,8 @@ function changeBgOfElementTo(element, color) {
 	if (color === 'cinder') localColor = 'hsl(235, 21%, 11%)';
 	$(element).style.backgroundColor = localColor;
 }
-function changeColorOfElementTo(element, color) {
+
+function setColorOfElementTo(element, color, multiple = false) {
 	// light theme
 	let localColor;
 	if (color === 'white') localColor = 'hsl(0, 0%, 100%)';
@@ -60,10 +64,14 @@ function changeColorOfElementTo(element, color) {
 	if (color === 'cinder') localColor = 'hsl(235, 21%, 11%)';
 	// console.log($(element));
 
-	$(element).style.color = localColor;
+	multiple
+		? document.querySelectorAll(`${element}`).forEach(el => {
+				el.style.color = localColor;
+		  })
+		: ($(element).style.color = localColor);
 }
 
-function changeBorderBottomOfElementsTo(element, color) {
+function setBorderBottomOfElementsTo(element, color) {
 	let innerColor;
 
 	if (color === 'mischka') innerColor = 'hsl(233, 11%, 84%)';
@@ -74,7 +82,7 @@ function changeBorderBottomOfElementsTo(element, color) {
 	});
 }
 
-function changeBorderOfElementTo(element, color) {
+function setBorderOfElementTo(element, color) {
 	let innerColor;
 
 	if (color === 'mischka') innerColor = 'hsl(233, 11%, 84%)';
@@ -88,48 +96,49 @@ function changeBorderOfElementTo(element, color) {
 function makeElementsLight() {
 	currentTheme = 'light';
 
-	changeBgOfElementTo('main', 'alabaster');
-	changeBgOfElementTo('.new-todo', 'alabaster');
-	changeBgOfElementTo('.new-con', 'alabaster');
-	changeBgOfElementTo('.show', 'alabaster');
-	changeBgOfElementTo('body', 'snuff');
-	changeColorOfElementTo('main', 'mulledwine');
-	changeColorOfElementTo('.new-todo', 'mulledwine');
+	setBgOfElementTo('main', 'alabaster');
+	setBgOfElementTo('.new-todo', 'alabaster');
+	setBgOfElementTo('.new-con', 'alabaster');
+	setBgOfElementTo('.show', 'alabaster');
+	setBgOfElementTo('body', 'snuff');
+	setColorOfElementTo('main', 'mulledwine');
+	setColorOfElementTo('.new-todo', 'mulledwine');
 
 	$('.new-todo').style.setProperty('--c', 'hsl(236, 9%, 61%)');
 
-	changeColorOfElementTo('.summary', 'manatee');
-	changeColorOfElementTo('.show span', 'manatee');
-	changeColorOfElementTo('.drag-n-drop-con', 'manatee');
-	changeColorOfElementTo('.logo', 'white');
-	changeColorOfElementTo('.done-todo-text', 'mischka');
+	setColorOfElementTo('.summary', 'manatee');
+	setColorOfElementTo('.show span', 'manatee');
+	setColorOfElementTo('.drag-n-drop-con', 'manatee');
+	setColorOfElementTo('.logo', 'white');
+	setColorOfElementTo('.done-todo-text', 'mischka');
+	setColorOfElementTo('.tabs', 'waterloo', true);
 
-	changeBorderBottomOfElementsTo('li', 'mischka');
-	changeBorderOfElementTo('.empty-circle', 'mischka');
+	setBorderBottomOfElementsTo('li', 'mischka');
+	setBorderOfElementTo('.empty-circle', 'mischka');
 }
 
 function makeElementsDark() {
 	currentTheme = 'dark';
 
-	changeBgOfElementTo('main', 'ebonyclay');
-	changeBgOfElementTo('.new-todo', 'ebonyclay');
-	changeBgOfElementTo('.new-con', 'ebonyclay');
-	changeBgOfElementTo('.show', 'ebonyclay');
-	changeBgOfElementTo('body', 'cinder');
-	changeColorOfElementTo('main', 'periwinkle');
-	changeColorOfElementTo('.new-todo', 'periwinkle');
+	setBgOfElementTo('main', 'ebonyclay');
+	setBgOfElementTo('.new-todo', 'ebonyclay');
+	setBgOfElementTo('.new-con', 'ebonyclay');
+	setBgOfElementTo('.show', 'ebonyclay');
+	setBgOfElementTo('body', 'cinder');
+	setColorOfElementTo('main', 'periwinkle');
+	setColorOfElementTo('.new-todo', 'periwinkle');
 
 	$('.new-todo').style.setProperty('--c', 'hsl(237, 14%, 26%)');
 
-	changeColorOfElementTo('.summary', 'trout');
-	changeColorOfElementTo('.done-todo-text', 'trout');
-	changeColorOfElementTo('.drag-n-drop-con', 'trout');
-	changeColorOfElementTo('.show span', 'waterloo');
-	changeColorOfElementTo('.logo', 'white');
-	changeColorOfElementTo('body', 'cinder');
+	setColorOfElementTo('.summary', 'trout');
+	setColorOfElementTo('.done-todo-text', 'trout');
+	setColorOfElementTo('.drag-n-drop-con', 'trout');
+	setColorOfElementTo('.show span', 'waterloo');
+	setColorOfElementTo('.logo', 'white');
+	setColorOfElementTo('body', 'cinder');
 
-	changeBorderOfElementTo('.empty-circle', 'brightgray');
-	changeBorderBottomOfElementsTo('li', 'brightgray');
+	setBorderOfElementTo('.empty-circle', 'brightgray');
+	setBorderBottomOfElementsTo('li', 'brightgray');
 }
 
 function showSun() {
@@ -189,5 +198,58 @@ function setBackgroundPhoto(desktopOrMobile, lightOrDark) {
 	$('header img').src = `assets/images/bg-${desktopOrMobile}-${lightOrDark}.jpg`;
 }
 
+function handleTabSelection(e) {
+	tabs.forEach(tab => {
+		if (tab === e.target) {
+			// tab.classList.add('selected');
+			tab.style.color = 'hsl(220, 98%, 61%)';
+		} else {
+			tab.style.color = 'rgb(119, 122, 146)';
+			// tab.classList.remove('selected');
+		}
+	});
+}
+
+function createNewTodo(e) {
+	e.preventDefault();
+	// new li gets created with $('input').value as text content
+	const todoText = $('input').value;
+	todosContainer.insertBefore(newLi(todoText), todosContainer.firstChild);
+}
+
+function newLi(text) {
+	const li = document.createElement('li');
+	li.className = 'undone-todo-text';
+
+	let color;
+	currentTheme === 'light' ? (color = 'rgb(210, 211, 219)') : (color = 'rgb(57, 58, 76)');
+	const circle = `
+  width: 1.3rem;
+	height: 1.3rem;
+	border-radius: 50%;
+  border: 1px solid ${color};
+  `;
+	li.style.borderBottom = `1px solid ${color}`;
+	li.innerHTML = `
+          <div class="icon-text">
+            <div class="empty-circle" style="${circle}"></div>
+            <span class="undone-todo-text">${text}</span>
+          </div>
+          <div class="delete">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+            ><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+          </div>
+  `;
+	return li;
+}
+
+setPreferredColorScheme();
+
 themeToggle.addEventListener('click', handleThemeToggler, false);
 window.addEventListener('change', setPreferredColorScheme);
+
+tabs.forEach(tab => tab.addEventListener('click', handleTabSelection));
+form.addEventListener('submit', createNewTodo);
